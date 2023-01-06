a1179c9b1734c24ac1fa136f55d271d31473b6cc
Anant Thombre <anantthombre1999@gmail.com>
2023-01-06 22:47:41 +0530

    added javasript part 1 file

---
+++ Javascript Level One/JavascriptPart1.js
@@ -0,0 +1,58 @@
+alert("Hello World!")
+undefined
+// anything here is a comment
+undefined
+// Basic Data Types
+undefined
+10
+10
+10.2
+10.2
+-13.4
+-13.4
+/Strings
+undefined
+"Hello Everyone"
+'Hello Everyone'
+"10"
+'10'
+//Boolean
+undefined
+//Booleans
+undefined
+true
+true
+false
+false
+//undefined and null
+undefined
+undefined
+undefined
+null
+null
+clear()
+Console was cleared
+undefined
+2+2
+4
+2*2
+4
+5-1
+4
+3*2
+6
+10/2
+5
+2/5
+0.4
+2**5
+32
+15/14
+1.0714285714285714
+15%14
+1
+13%2
+1
+18%4
+2
+clear()
