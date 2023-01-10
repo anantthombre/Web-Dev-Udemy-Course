d05b158ec006d5b41379bce9ea27beb872468324
Anant Thombre <anantthombre1999@gmail.com>
2023-01-10 19:15:00 +0530

    added JavaScriptPart2 html and js files

---
+++ Javascript Level One/JavaScriptPart2.js
@@ -0,0 +1,4 @@
+alert("Welcome to your Bank!!!")
+var deposit = prompt("How much would you like to deposit to your account?")
+alert("You have deposited: "+deposit)
+console.log("You are a cool Person!!!");
