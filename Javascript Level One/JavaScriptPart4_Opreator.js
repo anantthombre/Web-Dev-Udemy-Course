f24c54e4f32abe4c68c093d55acbeb8684d26c9f
Anant Thombre <anantthombre1999@gmail.com>
2023-01-11 18:53:45 +0530

    added JavaScriptPart4_Opreator.js

---
+++ Javascript Level One/JavaScriptPart4_Opreator.js
@@ -0,0 +1,66 @@
+2 >1
+true
+2 > 2
+false
+3<2
+false
+2<3
+true
+ 2 >= 2;
+true
+1<= 3;
+true
+2 == 2
+true
+"username" == "username"
+true
+2 != 3
+true
+2 == "2" 
+true
+5 === 5
+true
+5 === "5"
+false
+5 !== "5"
+true
+5 !== 5
+false
+true == 1
+true
+true === 1
+false
+false == 0
+true
+false === 0
+false
+null == undefined
+true
+null === undefined
+false
+NaN == NaN
+false
+NaN === NaN
+false
+1 === 1 && 2 === 2
+true
+1 === 2 || 1 === 1
+true
+!(1===1)
+false
+!!(1===1)
+true
+var x = 1
+undefined
+var y = 2
+undefined
+"2" == y && x === 1
+true
+x >= 0 || y === "2"
+true
+!(x !== 1) && y === (1+1)
+true
+y !== "2" && x < 10
+true
+y !== x || y == "2" || x === 3
+true
