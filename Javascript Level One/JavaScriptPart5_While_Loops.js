a9f701c165c2d6949efb74a2c81c7d72da85febd
Anant Thombre <anantthombre1999@gmail.com>
2023-01-12 00:10:35 +0530

    added JavaScriptPart5_While_Loops.html and JavaScriptPart5_While_Loops.js

---
+++ Javascript Level One/JavaScriptPart5_While_Loops.js
@@ -0,0 +1,24 @@
+// alert("Hello Everyone")
+// var x = 0;
+
+// while(x < 5){
+//     console.log("x is currently: "+ x);
+
+//     if (x === 3) {
+//         console.log("X is equal to three!")
+//         break;
+//     }
+//     console.log("x is still less than 5, adding 1 to x");
+//     x = x + 1;
+// }
+
+// Print only the even number 1 to 10
+
+x = 1;
+
+while (x < 11) {
+    if (x % 2 === 0) {
+        console.log(x);
+    }
+    x = x+1;
+}
