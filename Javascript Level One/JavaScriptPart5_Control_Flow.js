6e8390549c832dc2bc6dab3d1387ec17415e7804
Anant Thombre <anantthombre1999@gmail.com>
2023-01-11 23:48:28 +0530

    added 	JavaScriptPart5_Control_Flow.html and JavaScriptPart5_Control_Flow.js

---
+++ Javascript Level One/JavaScriptPart5_Control_Flow.js
@@ -0,0 +1,49 @@
+// alert("Hello Everyone!!!")
+// var hot = false
+// var temp = 12
+
+// if (true) {
+//     hot=true
+// }
+
+// console.log(hot)
+
+// if (temp>80) {
+//     console.log("Temp is greater than 80");
+//     hot = true
+// }
+
+// console.log(hot);
+
+// if (temp>80) {
+//     console.log("Hot Outside!");
+// }else {
+//     console.log("It's not very hot today!");
+// }
+
+// if (temp > 80) {
+//     console.log("Hot Outside!");
+// }else if (temp <= 80 && temp >=  50) {
+//     console.log("Average temp Outside");
+// }else if (temp < 50 && temp >= 32) {
+//     console.log("It's pretty cold out!")
+// }else {
+//     console.log("It's very very cold out!")
+// }
+
+var ham = 10 
+var cheese = 40
+
+var report = "blank";
+
+
+if (ham >= 10 && cheese >= 10) {
+    report = "Strong sales of ham and cheese"
+}else if (ham === 0 && cheese === 0) {
+    report = "Nothing Sold!"
+}else {
+    report = "We had some sales of items"
+}
+
+console.log(report);
+
