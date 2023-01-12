cdabbae31e26305c88db552f45b956af9b0f7727
Anant Thombre <anantthombre1999@gmail.com>
2023-01-12 14:08:19 +0530

    JavaScriptPart7_For_Loops.html and JavaScriptPart7_For_Loops.js

---
+++ Javascript Level One/JavaScriptPart7_For_Loops.js
@@ -0,0 +1,18 @@
+// alert("Hello Everyone!!!")
+
+// for (i = 0; i < 5; i++) {
+//     console.log("The current number is: " + i);
+// }
+
+// var word = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// for (index = 0; index < word.length; index++) {
+//     console.log(word[index]);
+// }
+
+var word = "ABABABABAAAAABANANA"
+
+for (index = 0; index < word.length; index += 2) {
+    console.log(word[index])
+}
+
