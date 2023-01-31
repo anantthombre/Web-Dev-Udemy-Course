06a35e13c7282871ed48910112d60aa591d3825b
Anant Thombre <anantthombre1999@gmail.com>
2023-01-31 19:02:32 +0530

    updated Document_Object_Model_P3_Events.js

--- Document Object Model/Document_Object_Model_P3_Events.js
+++ Document Object Model/Document_Object_Model_P3_Events.js
@@ -1,5 +1,25 @@
-var headOne = document.querySelector("#one")
-var headTwo = document.querySelector("#two")
-var headThree = document.querySelector("three")
+var headOne = document.querySelector("#one");
+var headTwo = document.querySelector("#two");
+var headThree = document.querySelector("#three");
 
-console.log("Connected!!!")
+// console.log("Connected!!!")
+
+ headOne.addEventListener("mouseover", function () {
+    headOne.textContent = "Mouse Currently Over";
+    headOne.style.color = "red";
+ })
+
+ headOne.addEventListener("mouseout", function(){
+    headOne.textContent = "HOVER OVER ME";
+    headOne.style.color = "black";
+ })
+
+ headTwo.addEventListener("click", function () {
+   headTwo.textContent = "CLICKED ON!";
+   headTwo.style.color = "blue";
+ })
+
+ headThree.addEventListener("dblclick", function () {
+   headThree.textContent = "DOUBLE CLICKED";
+   headThree.style.color = "green";
+ })
