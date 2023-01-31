6673d86da0b07cd8643a2ccf336ad345bda6156f
Anant Thombre <anantthombre1999@gmail.com>
2023-01-31 23:36:14 +0530

    updated  Document_Object_Model_Project.js

--- Document Object Model/Document_Object_Model_Project.js
+++ Document Object Model/Document_Object_Model_Project.js
@@ -18,6 +18,18 @@ restart.addEventListener("click", clearBoard);
 
 // Check the square maker
 
-
+function changeMarker(){
+    if(this.textContent === "") {
+        this.textContent = "X";
+    }else if (this.textContent === "X") {
+        this.textContent = "O";
+    }else {
+        this.textContent = "";
+    }
+}
 
 // For Loop to add event listners to all the squares
+
+for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("click", changeMarker)
+}
