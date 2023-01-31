0e301775662a556de6048f99c2a56584e2e4a900
Anant Thombre <anantthombre1999@gmail.com>
2023-01-31 22:53:32 +0530

    updated Document_Object_Model_Project.js

--- Document Object Model/Document_Object_Model_Project.js
+++ Document Object Model/Document_Object_Model_Project.js
@@ -1,1 +1,23 @@
-console.log("Connected");
+// Grab the restart button
+
+var restart = document.querySelector("#b");
+
+// Grab all the squares
+
+var squares = document.querySelectorAll("td");
+
+// Clear all the squares
+
+function clearBoard() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].textContent = "";
+    }
+}
+
+restart.addEventListener("click", clearBoard);
+
+// Check the square maker
+
+
+
+// For Loop to add event listners to all the squares
