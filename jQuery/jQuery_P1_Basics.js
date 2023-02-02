9009457d1a0069f87ce94aa334b85daf6d2c5565
Anant Thombre <anantthombre1999@gmail.com>
2023-02-03 00:03:37 +0530

    updated jQuery_P1_Basics.js

--- jQuery/jQuery_P1_Basics.js
+++ jQuery/jQuery_P1_Basics.js
@@ -22,3 +22,21 @@ var newCSS = {
 undefined
 x.css(newCSS)
 jQuery.fn.init [h1, prevObject: jQuery.fn.init(1)]
+var newCSS = {
+    "color" : "white",
+    'backgroud' : 'green',
+    'border' : '20px solid red'
+}
+undefined
+x.css(newCSS)
+jQuery.fn.init [h1, prevObject: jQuery.fn.init(1)]
+var listItems = $('li')
+undefined
+listItems
+jQuery.fn.init(6) [li, li, li, li, li, li#special, prevObject: jQuery.fn.init(1)]
+listItems.css('color','blue')
+jQuery.fn.init(6) [li, li, li, li, li, li#special, prevObject: jQuery.fn.init(1)]
+listItems.eq(0).css('color', 'orange')
+jQuery.fn.init [li, prevObject: jQuery.fn.init(6)]
+listItems.eq(-1).css('color','orange')
+jQuery.fn.init [li#special, prevObject: jQuery.fn.init(6)]
