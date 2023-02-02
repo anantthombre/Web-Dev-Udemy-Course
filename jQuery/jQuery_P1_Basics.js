3a585c11e388acc3b4121daf34bbe24c548ea5cf
Anant Thombre <anantthombre1999@gmail.com>
2023-02-02 23:53:55 +0530

    updated jQuery_P1_Basics.js

---
+++ jQuery/jQuery_P1_Basics.js
@@ -0,0 +1,16 @@
+$
+ƒ ( selector, context ) {
+
+		// The jQuery object is actually just the init constructor 'enhanced'
+		// Need init if jQuery is called (just allow error to be thrown if not included)
+		return new jQuery…
+$("h1")
+jQuery.fn.init [h1, prevObject: jQuery.fn.init(1)]
+$("li")
+jQuery.fn.init(6) [li, li, li, li, li, li#special, prevObject: jQuery.fn.init(1)]
+var x = $("h1")
+undefined
+x.css("color", "red")
+jQuery.fn.init [h1, prevObject: jQuery.fn.init(1)]
+x.css("background", "blue")
+jQuery.fn.init [h1, prevObject: jQuery.fn.init(1)]
