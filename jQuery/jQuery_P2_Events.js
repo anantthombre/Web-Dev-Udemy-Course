31168805d63047fb5f05df35f1d4217ded0eeea1
Anant Thombre <anantthombre1999@gmail.com>
2023-02-04 23:33:20 +0530

    updated jQuery_P2_Events.js

--- jQuery/jQuery_P2_Events.js
+++ jQuery/jQuery_P2_Events.js
@@ -27,6 +27,10 @@ $('h1').on('mouseenter', function(){
     $(this).toggleClass('turnBlue');
 })
 
+// $('input').eq(1).on('click', function () {
+//     $('.container').fadeOut(3000);
+// })
+
 $('input').eq(1).on('click', function () {
-    $('.container').fadeOut(3000);
+    $('.container').slideUp(3000);
 })
