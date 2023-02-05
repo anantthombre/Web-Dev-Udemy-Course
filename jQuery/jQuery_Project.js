ec3aa8035b47b053a5ba92af7bbe0a1e9e1377bc
Anant Thombre <anantthombre1999@gmail.com>
2023-02-05 19:00:39 +0530

    updated jQuery_Project.js

---
+++ jQuery/jQuery_Project.js
@@ -0,0 +1,10 @@
+var player1 = prompt("Player One: Enter Your Name, You Will Be Blue");
+var player1ColorClass = "p-1 w-24 h-24 rounded-full bg-gradient-to-bl from-blue-900 via-blue-400 to-blue-900";
+
+var player2 = prompt("Player Two: Enter Your Name, You Will Be Red");
+var player2ColorClass = "p-1 w-24 h-24 rounded-full bg-gradient-to-bl from-red-900 via-red-400 to-red-900";
+
+
+var game_on = true;
+var table = $("table tr");
+
