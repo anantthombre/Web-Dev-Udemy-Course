91ac579f61df989d792549e385bbedff4f274072
Anant Thombre <anantthombre1999@gmail.com>
2023-02-10 19:22:05 +0530

    updated numbers.py

--- Python Level One/numbers.py
+++ Python Level One/numbers.py
@@ -1,1 +1,22 @@
 print(2+1)
+print(2-1)
+print(2*2) 
+print(2/2)
+print(2**5)
+print(4**0.5)
+a = 5
+print(a + a) #Will print 10
+
+a = a*5
+#Will print 25
+print(a)
+
+two_dogs = 2 #Snake Casing. Used in Python
+twoDogs = 2 #Camel Casing. Used in JavaScript
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
+
+
