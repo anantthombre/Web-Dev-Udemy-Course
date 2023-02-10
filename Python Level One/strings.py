f2f767f5e068bf35931f93390fea4edea291252f
Anant Thombre <anantthombre1999@gmail.com>
2023-02-10 20:13:49 +0530

    added strings.py

---
+++ Python Level One/strings.py
@@ -0,0 +1,21 @@
+#Basics
+
+my_string = "abcdefg"
+print(my_string)
+
+#Indexing
+
+print(my_string[0])
+print(my_string[-1])
+print(my_string[3])
+
+#Slicing
+
+print(my_string[4:])
+print(my_string[:3])
+print(my_string[2:5])
+print(my_string[::2])
+print(my_string[1:6:2])
+
+#Basic Methods
+#Print Formatting
