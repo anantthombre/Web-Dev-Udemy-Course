2a9d5c86a0b26cb01fd399aca3b8a233960f896d
Anant Thombre <anantthombre1999@gmail.com>
2023-02-12 12:46:56 +0530

    updated lists.py

--- Python Level One/lists.py
+++ Python Level One/lists.py
@@ -5,3 +5,14 @@ print(my_list)
  
 my_list1 = [";ajsfahf;n", 1, 2.2, True, 'asdf', [1,2,3,4]]
 print(my_list1)
+
+print(len(my_list))
+print(len(my_list1))
+
+#Lists are mutable
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8]
+print("before assignment: ", my_list)
+
+my_list[0] = "New Item"
+print("afrer assignment: ", my_list)
