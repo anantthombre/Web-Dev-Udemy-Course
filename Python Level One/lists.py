dffbd277ef78c88a891b423e0e01a33f4013ea36
Anant Thombre <anantthombre1999@gmail.com>
2023-02-12 13:46:32 +0530

    updated lists.py

--- Python Level One/lists.py
+++ Python Level One/lists.py
@@ -16,3 +16,16 @@ print("before assignment: ", my_list)
 
 my_list[0] = "New Item"
 print("afrer assignment: ", my_list)
+
+#Appeding items to the list
+
+my_list.append("Appended Item")
+print(my_list)
+
+my_list.append(["Appended List", 3, 4])
+print(my_list)
+
+#Extending a List using another list
+
+my_list.extend([9, 10, 11, 12])
+print(my_list)
