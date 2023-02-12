bfda4b873ada7f9e2d86283702ca5d1675b0ce4d
Anant Thombre <anantthombre1999@gmail.com>
2023-02-12 18:45:10 +0530

    updated tuple_sets_booleans.py

--- Python Level One/tuple_sets_booleans.py
+++ Python Level One/tuple_sets_booleans.py
@@ -1,5 +1,33 @@
 # Booleans
+
 True
 False
 0
 1
+
+# Tuples
+
+t = (1, 2, 3)
+print(t[0])
+
+t = ('a', True, 123)
+print(t)
+
+# Tuples are immutable
+# t[0] = "New Item"  
+# Above line throws following error - TypeError: 'tuple' object does not support item assignment
+
+# Sets (Unordered collection of unique elements)
+
+x = set()
+
+x.add(0)
+x.add(1)
+x.add(4)
+x.add(0.1)
+x.add(4)
+x.add(4)
+print(x)
+
+converted = set([1, 1, 1, 1, 2, 2 ,3, 3, 3, 3, 5, 5, 5, 5])
+print(converted)
