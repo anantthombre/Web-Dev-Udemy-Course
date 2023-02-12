01bb0a963dff5c400271c8c815f813dbb39b0cbf
Anant Thombre <anantthombre1999@gmail.com>
2023-02-12 14:45:45 +0530

    updated lists.py

--- Python Level One/lists.py
+++ Python Level One/lists.py
@@ -29,3 +29,22 @@ print(my_list)
 
 my_list.extend([9, 10, 11, 12])
 print(my_list)
+
+#To remove an item from a list. pop() method
+
+my_list.pop()
+print(my_list)
+
+item = my_list.pop(0)
+print(my_list)
+print(item)
+
+#reverse and sort method
+my_list = [8, 5, 7, 3, 1, 6, 9, 2]
+print(my_list)
+
+my_list.reverse()
+print(my_list)
+
+my_list.sort()
+print(my_list)
