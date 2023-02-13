3f4ddfa5dace2254ebfd16907e70249903cde78d
Anant Thombre <anantthombre1999@gmail.com>
2023-02-13 22:47:33 +0530

    updated control_flow.py

--- Python Level One/control_flow.py
+++ Python Level One/control_flow.py
@@ -27,3 +27,33 @@
 
 # Multiple Logical Operators
 (1 == 2) or (2 == 3) or (4 == 4)
+
+
+# if & elif statements
+
+if 1 < 2:
+    print("First Block")
+    if 20 < 3:
+        print("Second Block")
+
+if 1 > 2:
+    print("Hello")
+else:
+    print("last")
+
+
+if 1 > 2:
+    print("Hello")
+elif 3 == 3:
+    print("elif statement ran")
+else:
+    print("last")
+
+
+if 1 == 1:
+    if 1 > 2:
+        print("Hello")
+    elif 3 == 3:
+        print("elif statement ran")
+    else:
+        print("last")
