47d51fe38055bd3e8884d9d76d9fafe4afb7e3bd
Anant Thombre <anantthombre1999@gmail.com>
2023-02-13 21:22:59 +0530

    updated control_flow.py

---
+++ Python Level One/control_flow.py
@@ -0,0 +1,29 @@
+# Comparison Operator
+
+1 > 2
+1 < 2
+1 >= 1
+1 <= 1
+1 <= 4
+
+# Equality
+
+1 == 1
+1 == "1"
+"hi" == "bye"
+
+# Inequality
+
+1 != 2
+
+
+# Logical Operators
+
+# AND
+(1 > 2) and (2 < 3)
+
+# OR
+(1 > 2) or (2 < 3)
+
+# Multiple Logical Operators
+(1 == 2) or (2 == 3) or (4 == 4)
