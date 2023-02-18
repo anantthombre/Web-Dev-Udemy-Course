e2f149543dacb5e17fcf1ede9ad020e2a9959ca4
Anant Thombre <anantthombre1999@gmail.com>
2023-02-18 20:28:03 +0530

    added name_and_main_one.py &  name_and_main_two.py

---
+++ Python Level Two/name_and_main_one.py
@@ -0,0 +1,11 @@
+def func():
+    print("func() in name_and_main_one.py")
+
+print("TOP LEVEL NAME_AND_MAIN_ONE.PY")
+
+if __name__ == "__main__":
+    print("name_and_main_one.py is being run directly")
+else:
+    print("name_and_main_one.py is being imported")
+
+
---
+++ Python Level Two/name_and_main_two.py
@@ -0,0 +1,9 @@
+import name_and_main_one
+
+print("TOP LEVEL NAME_AND_MAIN_TWO.PY")
+
+if __name__ == "__main__":
+    print("name_and_main_two.py is being run directly")
+else:
+    print("name_and_main_two.py is being imported")
+
