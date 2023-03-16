9ff56bab60de171f5a505bf4327603a72c8e29eb
Anant Thombre <anantthombre1999@gmail.com>
2023-03-16 15:57:17 +0530

    added lots of files

--- Django Level One/project_two/project_two/project_two/settings.py
+++ Django Level One/project_two/project_two/project_two/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7!x!oe@7matp&847$nu-53bt+e$g^^oa#tmps2ms7t0u*y9_u0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
