35a8ba1a51fd96930ff72254bf4bda12920a3896
Anant Thombre <anantthombre1999@gmail.com>
2023-03-22 19:08:36 +0530

    updated settings.py

--- Django Level One/first_project/first_project/settings.py
+++ Django Level One/first_project/first_project/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# print(BASE_DIR)
+print(BASE_DIR)
 print(os.path.abspath(__file__))
 
 
