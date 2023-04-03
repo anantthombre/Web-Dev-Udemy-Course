4682d2146868a77263d81e6d3b8a6aff14921c48
Anant Thombre <anantthombre1999@gmail.com>
2023-04-03 17:25:38 +0530

    updated pro_two settings.py

--- Django Level One/project_two/project_two/settings.py
+++ Django Level One/project_two/project_two/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # print(Path(__file__).resolve())
-TEMPLATE_DIR = Path.join(BASE_DIR,"templates")
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 print(TEMPLATE_DIR)
 
 
