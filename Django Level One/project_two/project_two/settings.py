c0ff749823ca0d297daeb7b6d6f1b697c393ccc4
Anant Thombre <anantthombre1999@gmail.com>
2023-04-03 16:40:57 +0530

    updated settings.pys

--- Django Level One/project_two/project_two/settings.py
+++ Django Level One/project_two/project_two/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# print(Path(__file__).resolve())
+TEMPLATE_DIR = Path.join(BASE_DIR,"templates")
+print(TEMPLATE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
