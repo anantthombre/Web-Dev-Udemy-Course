45cae0115586d256cf2d0f5f01445ccaf33ba282
Anant Thombre <anantthombre1999@gmail.com>
2023-04-14 23:57:58 +0530

    updated populate_first_app.py

--- Django Level Two/first_project/populate_first_app.py
+++ Django Level Two/first_project/populate_first_app.py
@@ -4,3 +4,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
 import django
 django.setup()
 
+#FAKE POP SCRIPT
+import random
+from first_app.models import AccessRecord, Webpage, Topic
+from faker import Faker
+
+
+fakegen = Faker()
+topics = ['Search', 'Social', 'Markertplace', 'News', 'Games']
+
+def add_topic():
+    t = Topic.objects.get_or_create(top_name= random.choice(topics))[0]
