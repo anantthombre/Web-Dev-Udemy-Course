ff912b5b94f55a5bb7167b257838d1536ddc2e87
Anant Thombre <anantthombre1999@gmail.com>
2023-04-15 00:02:52 +0530

    updated populate_first_app.py

--- Django Level Two/first_project/populate_first_app.py
+++ Django Level Two/first_project/populate_first_app.py
@@ -15,3 +15,17 @@ topics = ['Search', 'Social', 'Markertplace', 'News', 'Games']
 
 def add_topic():
     t = Topic.objects.get_or_create(top_name= random.choice(topics))[0]
+    t.save()
+    return t
+
+def populate(N=5):
+    for entry in range(N):
+        top = add_topic() # Get the topic for the entry
+
+        # Create the fake data for the entry
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+
+        
