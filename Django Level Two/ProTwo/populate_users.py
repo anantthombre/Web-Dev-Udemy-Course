58bf2a609525a0ade5c664cb46c56b0b786726e3
Anant Thombre <anantthombre1999@gmail.com>
2023-04-16 00:32:41 +0530

    updated populate_users.py

--- Django Level Two/ProTwo/populate_users.py
+++ Django Level Two/ProTwo/populate_users.py
@@ -10,11 +10,13 @@ from faker import Faker
 fakegen = Faker()
 
 def populate(N=5):
-    fake_first_name, fake_last_name = fakegen.name().split()
-    fake_email = fakegen.email()
+    for entry in range(N):
 
-    # New entry in Database
-    user = User.objects.get_or_create(first_name = fake_first_name, last_name = fake_last_name, email = fake_email)[0]
+        fake_first_name, fake_last_name = fakegen.name().split()
+        fake_email = fakegen.email()
+
+        # New entry in Database
+        user = User.objects.get_or_create(first_name = fake_first_name, last_name = fake_last_name, email = fake_email)[0]
 
 
 if __name__ == "__main__":
