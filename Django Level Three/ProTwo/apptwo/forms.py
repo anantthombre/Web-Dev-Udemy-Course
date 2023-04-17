4b17150bef7499f7435148dab3662fb7b087592f
Anant Thombre <anantthombre1999@gmail.com>
2023-04-17 20:02:02 +0530

    updated forms.py and views.py

--- Django Level Three/ProTwo/apptwo/forms.py
+++ Django Level Three/ProTwo/apptwo/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from apptwo.models import User
 
-class NewUser(forms.ModelForm):
+class NewUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = "__all__"
--- Django Level Three/ProTwo/apptwo/views.py
+++ Django Level Three/ProTwo/apptwo/views.py
@@ -6,7 +6,7 @@ def index(request):
     return render(request, 'apptwo/index.html')
 
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users': user_list}
-    return render(request, 'apptwo/user.html', context=user_dict)
+    # user_list = User.objects.order_by('first_name')
+    # user_dict = {'users': user_list}
+    # return render(request, 'apptwo/user.html', context=user_dict)
 
