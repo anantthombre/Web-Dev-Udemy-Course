0dfb717338807ee29702a019d375b7fac463c1cb
Anant Thombre <anantthombre1999@gmail.com>
2023-04-20 21:54:56 +0530

    updated decorators.py

--- Python Level Two/decorators.py
+++ Python Level Two/decorators.py
@@ -8,3 +8,25 @@ def func():
 
 
 func()
+
+
+def hello(name="Jose"):
+    return "Hello " + name
+
+print(hello())
+
+mynewgreet = hello
+
+print(mynewgreet())
+
+def hello(name = "Jose"):
+    print("The Hello() function has been run!")
+
+    def greet():
+        return "This string is inside greet()"
+    
+    def welcome(): return "This string is inside welcome()"
+
+
+
+hello()
