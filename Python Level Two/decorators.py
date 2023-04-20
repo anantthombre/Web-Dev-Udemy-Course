8586f08ccb091e6843888c3d55b1bbfd6492ad95
Anant Thombre <anantthombre1999@gmail.com>
2023-04-20 23:42:58 +0530

    updated decorators.py

--- Python Level Two/decorators.py
+++ Python Level Two/decorators.py
@@ -27,6 +27,59 @@ def hello(name = "Jose"):
     
     def welcome(): return "This string is inside welcome()"
 
+    print(greet())
+    print(welcome())
+    print("End of Hello() function")
+
 
 
 hello()
+
+
+
+def hello(name = "Jose"):
+    print("The Hello() function has been run!")
+
+    def greet():
+        return "This string is inside greet()"
+    
+    def welcome(): return "This string is inside welcome()"
+
+    if name == "Jose":
+        return greet
+    else:
+        return welcome
+    
+x = hello()
+
+print(x())
+
+def hello():
+    return "Hi Jose!"
+
+def other(func):
+    print("Hello")
+    # print(func)
+    print(func())
+
+other(hello)
+
+
+def new_decorator(func):
+
+    def wrap_func():
+        print("Code Here Before Executing func")
+        func()
+        print("Func() has been called")
+
+    return wrap_func
+
+
+@new_decorator
+def func_needs_decorator():
+    print("This function is need in the decorator!")
+
+# func_needs_decorator()
+# func_needs_decorator = new_decorator(func_needs_decorator)
+
+func_needs_decorator()
